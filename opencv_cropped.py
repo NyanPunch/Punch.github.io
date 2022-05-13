9d430bdd394ace6925fb5fd57592deafcc0fcad9
WhoLook <51149853+NyanPunch@users.noreply.github.com>
2022-05-13 15:45:16 +0900

    Update opencv_cropped.py

--- opencv_cropped.py
+++ opencv_cropped.py
@@ -5,14 +5,14 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 # 이미지 불러오기
-img = cv2.imread('image.jpg') # 이미지 파일 삽입 부분
+img = cv2.imread('sisa.jpg') # 이미지 파일 삽입 부분
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # 얼굴 찾기
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
     cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0),2)
-    cropped = img[y - int(h/4):y + h + int(h/4), x - int(w/4):x + w + int(w/4)]
+    cropped = img[y - int(h/4):y + h + int(h/1), x - int(w/4):x + w + int(w/4)] #상:하, 좌:우
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
     eyes = eye_cascade.detectMultiScale(roi_gray)
